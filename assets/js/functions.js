8b4606279e5580a788f3604f7c4e6bac3ecb613a
wushuyi <wushuyi541@gmail.com>
2014-07-24 19:48:18 +0800

    完成基本,收摊回家

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -11,6 +11,16 @@
 			js.type = 'text/javascript';
 			js.src = scriptSrc;
 			s.parentNode.insertBefore(js, s);
+		},
+		merge : function($obj1, $obj2) {
+			var arr = [];
+			$obj1.each(function(i) {
+				arr.push(this);
+			});
+			$obj2.each(function(i) {
+				arr.push(this);
+			});
+			return $(arr);
 		}
 	};
 })('pubfun', this);
@@ -76,7 +86,7 @@
 						var thisCss = {};
 						thisCss = _THIS.getCss(e, thisObj);
 						//$runobj.eq(i).css(thisCss);
-						TweenMax.from(thisDom, 5, thisCss);
+						TweenMax.from(thisDom, 1, thisCss);
 					}
 				});
 			});
@@ -173,138 +183,259 @@ var $objCache = {};
 (function(puma, constructor) {
 	var $pg = $objCache;
 	var pgindex = {};
+	var $pgbox = $('div.pg');
+	pgindex.thisIndex = 0;
+	pgindex.lock = false;
+	pgindex.max = $pgbox.size();
+
+	// 动画序列
+	pgindex.action = {
+		object : {
+			pg1cent1 : $('.pg1 .cent1'),
+			pg1cent2 : $('.pg1 .cent2'),
+			pg1tit : $('.pg1 .tit'),
+			pg2tit : $('.pg2 .tit')
+		},
+		thisInit : {
+			0 : function() {
+				var domList = pgindex.action.object;
+				domList.pg1cent1.css({
+					'left' : '-80px',
+					'opacity' : 0
+				});
+				domList.pg1cent2.css({
+					'left' : '80px',
+					'opacity' : 0
+				});
+				domList.pg1tit.css({
+					'margin-top' : '-20px',
+					'opacity' : 0
+				});
+			},
+			1 : function() {
+				var domList = pgindex.action.object;
+				domList.pg2tit.css({
+					'margin-top' : '-20px',
+					'opacity' : 0
+				});
+			},
+			2 : function() {
+
+			},
+			3 : function() {
+
+			},
+			4 : function() {
+
+			},
+			5 : function() {
+
+			},
+			6 : function() {
+
+			}
+		},
+		thisIn : {
+			0 : function() {
+				var domList = pgindex.action.object;
+				TweenMax.to(domList.pg1cent1, 1, {
+					'left' : '0',
+					'opacity' : 1
+				});
+				TweenMax.to(domList.pg1cent2, 1, {
+					'left' : '0',
+					'opacity' : 1
+				});
+				TweenMax.to(domList.pg1tit, 1, {
+					'margin-top' : '0px',
+					'opacity' : 1
+				});
+			},
+			1 : function() {
+				var domList = pgindex.action.object;
+				TweenMax.to(domList.pg2tit, 1, {
+					'margin-top' : '0px',
+					'opacity' : 1
+				});
+			},
+			2 : function() {
+
+			},
+			3 : function() {
+
+			},
+			4 : function() {
+
+			},
+			5 : function() {
+
+			},
+			6 : function() {
+
+			}
+		},
+		thisOut : {
+			0 : function() {
+				var domList = pgindex.action.object;
+				var obj = pubfun.merge(domList.pg1cent1, domList.pg1cent2);
+				TweenMax.to(obj, 1, {
+					'opacity' : 0
+				});
+				TweenMax.to(domList.pg1tit, 1, {
+					'margin-top' : '20px',
+					'opacity' : 0
+				});
+			},
+			1 : function() {
+				var domList = pgindex.action.object;
+				TweenMax.to(domList.pg2tit, 1, {
+					'margin-top' : '20px',
+					'opacity' : 0
+				});
+			},
+			2 : function() {
+
+			},
+			3 : function() {
+
+			},
+			4 : function() {
+
+			},
+			5 : function() {
+
+			},
+			6 : function() {
+
+			}
+		}
+	};
+
+	pgindex.pgNext = function() {
+		var _THIS = this;
+		if (pgindex.lock) {
+			return false;
+		}
+		pgindex.lock = true;
+		var pgNext = pgindex.thisIndex + 1;
+		if (pgNext >= pgindex.max) {
+			pgNext = 0;
+		}
+		var $nextPg = $pgbox.eq(pgNext);
+		var $thisPg = $pgbox.eq(pgindex.thisIndex);
+		_THIS.action.thisInit[pgNext]();
+		_THIS.action.thisOut[pgindex.thisIndex]();
+		$thisPg.css({
+			'left' : '0%',
+			'z-index' : '9'
+		});
+		$nextPg.css({
+			'left' : '100%',
+			'z-index' : '10'
+		});
+		TweenMax.to($nextPg, 1, {
+			'left' : '0%'
+		});
+		TweenMax.to($thisPg, 1, {
+			'left' : '-25%',
+			onComplete : function() {
+				$nextPg.css({
+					'z-index' : '1'
+				}).addClass('run');
+				$thisPg.css({
+					'left' : '-100%',
+					'z-index' : '0'
+				}).removeClass('run');
+				pgindex.thisIndex = pgNext;
+				pgindex.lock = false;
+				_THIS.action.thisIn[pgNext]();
+			}
+		});
+
+	};
+	pgindex.pgPrev = function() {
+		var _THIS = this;
+		if (pgindex.lock) {
+			return false;
+		}
+		pgindex.lock = true;
+		var pgNext = pgindex.thisIndex - 1;
+		if (pgNext < 0) {
+			pgNext = pgindex.max - 1;
+		}
+		var $nextPg = $pgbox.eq(pgNext);
+		var $thisPg = $pgbox.eq(pgindex.thisIndex);
+		_THIS.action.thisInit[pgNext]();
+		_THIS.action.thisOut[pgindex.thisIndex]();
+		$thisPg.css({
+			'left' : '0%',
+			'z-index' : '9'
+		});
+		$nextPg.css({
+			'left' : '-100%',
+			'z-index' : '10'
+		});
+		TweenMax.to($nextPg, 1, {
+			'left' : '0%',
+		});
+		TweenMax.to($thisPg, 1, {
+			'left' : '25%',
+			onComplete : function() {
+				$nextPg.css({
+					'z-index' : '1'
+				}).addClass('run');
+				;
+				$thisPg.css({
+					'left' : '100%',
+					'z-index' : '0'
+				}).removeClass('run');
+				;
+				pgindex.thisIndex = pgNext;
+				pgindex.lock = false;
+				_THIS.action.thisIn[pgNext]();
+			}
+		});
+	};
+
 	constructor[puma] = {
+		test : pgindex,
 		pgsize : function() {
 			var pgH = $pg.win.height();
 			$pg.main.height(pgH);
 			$pg.mainBox.height(pgH - 50);
 		},
 		domEvent : function() {
-			var $pgbox = $('div.pg');
-			pgindex.thisIndex = 0;
-			pgindex.lock = false;
-			pgindex.max = $pgbox.size();
-			pgindex.pgNext = function() {
-				if (pgindex.lock) {
-					return false;
-				}
-				pgindex.lock = true;
-				var pgNext = pgindex.thisIndex + 1;
-				if (pgNext >= pgindex.max) {
-					pgNext = 0;
-				}
-				var $nextPg = $pgbox.eq(pgNext);
-				var $thisPg = $pgbox.eq(pgindex.thisIndex);
-				$thisPg.css({
-					'left' : '0%',
-					'z-index' : '9'
-				});
-				$nextPg.css({
-					'left' : '100%',
-					'z-index' : '10'
-				});
-				TweenMax.to($nextPg, 1, {
-					'left' : '0%'
-				});
-				TweenMax.to($thisPg, 1, {
-					'left' : '-25%',
-					onComplete : function() {
-						$nextPg.css({
-							'z-index' : '1'
-						}).addClass('run');
-						$thisPg.css({
-							'left' : '-100%',
-							'z-index' : '0'
-						}).removeClass('run');
-						pgindex.thisIndex = pgNext;
-						pgindex.lock = false;
-					}
-				});
-			};
-			pgindex.pgPrev = function() {
-				if (pgindex.lock) {
-					return false;
-				}
-				pgindex.lock = true;
-				var pgNext = pgindex.thisIndex - 1;
-				if (pgNext < 0) {
-					pgNext = pgindex.max - 1;
-				}
-				var $nextPg = $pgbox.eq(pgNext);
-				var $thisPg = $pgbox.eq(pgindex.thisIndex);
-				$thisPg.css({
-					'left' : '0%',
-					'z-index' : '9'
-				});
-				$nextPg.css({
-					'left' : '-100%',
-					'z-index' : '10'
-				});
-				TweenMax.to($nextPg, 1, {
-					'left' : '0%',
-				});
-				TweenMax.to($thisPg, 1, {
-					'left' : '25%',
-					onComplete : function() {
-						$nextPg.css({
-							'z-index' : '1'
-						}).addClass('run');;
-						$thisPg.css({
-							'left' : '100%',
-							'z-index' : '0'
-						}).removeClass('run');;
-						pgindex.thisIndex = pgNext;
-						pgindex.lock = false;
-					}
-				});
-			};
-
+			// 点击 按钮 翻页
 			$pg.pgNext.click(function() {
 				pgindex.pgNext();
 			});
 			$pg.pgPrev.click(function() {
 				pgindex.pgPrev();
 			});
-			
-			window._NUM_SCROLL=0;
-			$('body').mousewheel(function(e){
+			// 鼠标滚轮 翻页
+			window._NUM_SCROLL = 0;
+			$('body').mousewheel(function(e) {
 				(e.deltaY > 0) ? window._NUM_SCROLL += 1 : window._NUM_SCROLL -= 1;
-				if(window._NUM_SCROLL >= 5){
+				if (window._NUM_SCROLL >= 5) {
 					pgindex.pgPrev();
-					window._NUM_SCROLL=0;
-				}else if(window._NUM_SCROLL <= -5){
+					window._NUM_SCROLL = 0;
+				} else if (window._NUM_SCROLL <= -5) {
 					pgindex.pgNext();
-					window._NUM_SCROLL=0;
+					window._NUM_SCROLL = 0;
 				}
 				//console.log(window._NUM_SCROLL);
 			});
+			// 键盘左右 翻页
+			$(document).keydown(function(e) {
+				if (e.keyCode == 37 || e.keyCode == 65) {
+					pgindex.pgPrev();
+				} else if (e.keyCode == 39 || e.keyCode == 68) {
+					pgindex.pgNext();
+				}
+				//console.log(e.keyCode);
+			});
 		},
 		load : function() {
-			var cent1 = $('div.cent1');
-			var cent2 = $('div.cent2');
-			var main = $('div.mainpg');
-			var _THIS = this;
-			this.amin1 = function() {
-				TweenMax.from(cent1, 1, {
-					'left' : '-80px',
-					'opacity' : 0
-				});
-				TweenMax.from(cent2, 1, {
-					'left' : '80px',
-					'opacity' : 0
-				});
-			}
-			this.amin2 = function() {
-				TweenMax.from($pg.btnav, 1, {
-					'bottom' : '-50px',
-					'backgroundColor' : '#CCCCCC',
-					'opacity' : 0,
-					onComplete : function() {
-						_THIS.amin1();
-					}
-				});
-			}
-			_THIS.amin2();
 		}
 	};
 })('puma', this);
@@ -321,7 +452,6 @@ var $objCache = {};
 			pg1 : $('div.pg1'),
 			pgPrev : $('div.prev'),
 			pgNext : $('div.next')
-
 		}
 		$.extend($objCache, initobj);
 
